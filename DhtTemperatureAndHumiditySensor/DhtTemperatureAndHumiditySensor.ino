9724af9816154d716a2e484956d4fb775ccdfb0b
Markus Wild <markus@jona-Linux2.fritz.box>
2020-05-24 15:40:08 +0200

    1. Version

--- DhtTemperatureAndHumiditySensor/DhtTemperatureAndHumiditySensor.ino
+++ DhtTemperatureAndHumiditySensor/DhtTemperatureAndHumiditySensor.ino
@@ -1,5 +1,4 @@
-/*
- *
+/**
  * The MySensors Arduino library handles the wireless radio link and protocol
  * between your home built sensors/actuators and HA controller of choice.
  * The sensors forms a self healing radio network with optional repeaters. Each
@@ -36,18 +35,12 @@
 #define MY_DEBUG
 
 // Enable and select radio type attached 
-////#define MY_RADIO_RF24
+#define MY_RADIO_RF24
 //#define MY_RADIO_RFM69
 //#define MY_RS485
-#define MY_RADIO_RF24
-//#define MY_RF24_PA_LEVEL RF24_PA_LOW
-//#define MY_GATEWAY_SERIAL 1
-
-#include <config.h>
-#include <HX711_ADC.h>
+ 
 #include <SPI.h>
 #include <MySensors.h>  
-//#include <DHT_U.h>
 #include <DHT.h>
 
 // Set this to the pin you connected the DHT's data pin to
@@ -59,7 +52,7 @@
 
 // Sleep time between sensor updates (in milliseconds)
 // Must be >1000ms for DHT22 and >2000ms for DHT11
-static const uint64_t UPDATE_INTERVAL = 1000;
+static const uint64_t UPDATE_INTERVAL = 60000;
 
 // Force sending an update of the temperature after n sensor reads, so a controller showing the
 // timestamp of the last update doesn't show something like 3 hours in the unlikely case, that
@@ -69,7 +62,6 @@ static const uint8_t FORCE_UPDATE_N_READS = 10;
 
 #define CHILD_ID_HUM 0
 #define CHILD_ID_TEMP 1
-#define CHILD_ID_WEIGHT 2
 
 float lastTemp;
 float lastHum;
@@ -79,112 +71,43 @@ bool metric = true;
 
 MyMessage msgHum(CHILD_ID_HUM, V_HUM);
 MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
-MyMessage msgLoad(CHILD_ID_WEIGHT, V_WEIGHT);
 DHT dht;
 
-//-------------------------------------------------------------------------------------
-// HX711_ADC.h
-// Arduino master library for HX711 24-Bit Analog-to-Digital Converter for Weigh Scales
-// Olav Kallhovd sept2017
-// Tested with      : HX711 asian module on channel A and YZC-133 3kg load cell
-// Tested with MCU  : Arduino Nano, ESP8266
-//-------------------------------------------------------------------------------------
-// This is an example sketch on how to use this library
-// Settling time (number of samples) and data filtering can be adjusted in the config.h file
 
-//#include <EEPROM.h>
-
-//HX711 constructor (dout pin, sck pin):
-HX711_ADC LoadCell(4, 5);
-
-const int eepromAdress = 0;
-
-long t;
-
-
-void presentation()  
+void presentation_()  
 { 
   // Send the sketch version information to the gateway
   sendSketchInfo("TemperatureAndHumidity", "1.1");
-
+  
   // Register all sensors to gw (they will be created as child devices)
   present(CHILD_ID_HUM, S_HUM);
   present(CHILD_ID_TEMP, S_TEMP);
-  present(CHILD_ID_WEIGHT, S_WEIGHT);
-
+  
   metric = getControllerConfig().isMetric;
 }
 
-void setup() {
 
+void setup()
+{
   dht.setup(DHT_DATA_PIN); // set data pin of DHT sensor
   if (UPDATE_INTERVAL <= dht.getMinimumSamplingPeriod()) {
-    //Serial.println("Warning: UPDATE_INTERVAL is smaller than supported by the sensor!");
+    Serial.println("Warning: UPDATE_INTERVAL is smaller than supported by the sensor!");
   }
-  // Sleep for the time of the minimum sampling period to give the sensor time to powesr up
+  // Sleep for the time of the minimum sampling period to give the sensor time to power up
   // (otherwise, timeout errors might occure for the first reading)
   sleep(dht.getMinimumSamplingPeriod());
-
-  float calValue; // calibration value
-  calValue = 883.73; // uncomment this if you want to set this value in the sketch 
-  #if defined(ESP8266) 
-  //EEPROM.begin(512); // uncomment this if you use ESP8266 and want to fetch the value from eeprom
-  #endif
-  //EEPROM.get(eepromAdress, calValue); // uncomment this if you want to fetch the value from eeprom
-  
-  //Serial.begin(9600); delay(10);
-  Serial.println();
-  Serial.println("Starting...");
-  LoadCell.begin();
-  long stabilisingtime = 2000; // tare preciscion can be improved by adding a few seconds of stabilising time
-  LoadCell.start(stabilisingtime);
-  if(LoadCell.getTareTimeoutFlag()) {
-    Serial.println("Tare timeout, check MCU>HX711 wiring and pin designations");
-  }
-  else {
-    LoadCell.setCalFactor(calValue); // set calibration value (float)
-    Serial.println("Startup + tare is complete");
-  }
 }
 
-void loop() {
-  //update() should be called at least as often as HX711 sample rate; >10Hz@10SPS, >80Hz@80SPS
-  //use of delay in sketch will reduce effective sample rate (be carefull with use of delay() in the loop)
-  LoadCell.update();
 
-  //get smoothed value from data set
-  if (millis() > t + 250) {
-    float i = LoadCell.getData();
-#ifdef MY_DEBUG
-    
-    Serial.print("Load_cell output val: ");
-    Serial.println(i);
-#endif    
-    send(msgLoad.set(&i));
-    t = millis();
-  }
-
-  //receive from serial terminal
-  if (Serial.available() > 0) {
-    float i;
-    char inByte = Serial.read();
-    if (inByte == 't') LoadCell.tareNoDelay();
-  }
-
-  //check if last tare operation is complete
-  if (LoadCell.getTareStatus() == true) {
-#ifdef MY_DEBUG    
-    Serial.println("Tare complete");
-#endif    
-  }
-
+void loop()      
+{  
   // Force reading sensor, so it works also after sleep()
   dht.readSensor(true);
-
+  
   // Get temperature from DHT library
   float temperature = dht.getTemperature();
   if (isnan(temperature)) {
-    //Serial.println("Failed reading temperature from DHT!");
+    Serial.println("Failed reading temperature from DHT!");
   } else if (temperature != lastTemp || nNoUpdatesTemp == FORCE_UPDATE_N_READS) {
     // Only send temperature if it changed since the last measurement or if we didn't send an update for n times
     lastTemp = temperature;
@@ -209,17 +132,16 @@ void loop() {
   }
 
   // Get humidity from DHT library
-  //float humidity = dht.getHumidity();
-  float humidity=0.0;
+  float humidity = dht.getHumidity();
   if (isnan(humidity)) {
-    //Serial.println("Failed reading humidity from DHT");
+    Serial.println("Failed reading humidity from DHT");
   } else if (humidity != lastHum || nNoUpdatesHum == FORCE_UPDATE_N_READS) {
     // Only send humidity if it changed since the last measurement or if we didn't send an update for n times
     lastHum = humidity;
     // Reset no updates counter
     nNoUpdatesHum = 0;
     send(msgHum.set(humidity, 1));
-
+    
     #ifdef MY_DEBUG
     Serial.print("H: ");
     Serial.println(humidity);
